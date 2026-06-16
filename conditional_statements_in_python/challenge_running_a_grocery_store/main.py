537242ce47ab22ae96d8bc3d696c2f7752294934
way2geek11 <>
2026-06-16 04:14:32 +0000

    Challenge: Running a Grocery Store

---
+++ conditional_statements_in_python/challenge_running_a_grocery_store/main.py
@@ -0,0 +1,12 @@
+# Daily revenue of the grocery store and initial assumption
+revenue = 2000
+is_business_successful = False
+
+# Determine business success
+if revenue >= 2000:
+  is_business_successful = True
+else:
+  is_business_successful = False
+
+# Testing
+print("Is business hits the desired revenue?", is_business_successful)
